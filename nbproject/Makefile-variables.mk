f7f07455e4323905c73ffde082f389d5d18ab8fe
btondinhcpa <btondin@hcpa.edu.br>
2023-01-25 17:57:18 -0300

    Primeiro Commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LEDS.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LEDS.X.production.hex
